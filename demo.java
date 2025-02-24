85aee999964d934f46a6201fd3b4e2785c8fc9b3
Kamalesh D <kamaleshrockz@gmail.com>
2025-02-24 21:39:30 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main(String[] args) {
         // Print a message to the console as output
-        System.out.println("Hello welcome to jenkins, checking POLLSCM trigger");
+        System.out.println("Hello welcome to jenkins, checking github-webhook trigger");
     } 
 }
